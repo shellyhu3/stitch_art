0509f1021e00c9bc58e8a35e62587c480cccb0e2
Shelly <39971043+shellyhu3@users.noreply.github.com>
2019-07-10 16:21:36 -0700

    fixed small nav bug and map

--- apps/admin_app/static/admin_app/script.js
+++ apps/admin_app/static/admin_app/script.js
@@ -1,3 +1,4 @@
+//Navbar shrink on scroll
 $(document).on('scroll', () => scrollFunction());
 
 function scrollFunction(){  
@@ -15,21 +16,35 @@ function scrollFunction(){
 }
 
 $(document).ready(()=>{
-// Get the modal
-var modal = $("#myModal");
+    // Get the modal
+    var modal = $("#myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var modalImg = $("#img01");
-$(".img").click((e) =>{
-    console.log(e.target)
-    modal.css('display','block');
-    modalImg.attr('src', e.target.src);
-})
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var modalImg = $("#img01");
+    $(".img").on('click', (e) =>{
+        console.log(e.target)
+        modal.css('display','block');
+        modalImg.attr('src', e.target.src);
+    })
 
+    // When the user clicks on <span> (x), close the modal
+    $('.close').on('click', () => { 
+        modal.css('display','none');
+    })
 
-// When the user clicks on <span> (x), close the modal
-$('.close').click(() => { 
-    modal.css('display','none');
-})
+    $('#small_nav').hover(
+        () => $('.dropdown').addClass('show_dropdown'),
+        () => $('.dropdown').removeClass('show_dropdown')
+    )
 
+    $('#nav_icon').on('click', (e) => {
+        e.stopPropagation();
+        // $('#dropdown').addClass('show_nav');
+        // $('#small_nav').addClass('show_nav');
+        $('.dropdown').toggleClass('show_dropdown');
+    })
+
+    $(document).on('click', () => {
+        $('.dropdown').removeClass('show_dropdown');
+    })
 })
