906bfd6ac7208eda0371269f630219c370b813d0
Shelly Hu <shellyhu3@gmail.com>
2020-06-15 22:24:44 -0700

    new covid update & some style changes with top nav

--- apps/admin_app/static/admin_app/script.js
+++ apps/admin_app/static/admin_app/script.js
@@ -6,13 +6,13 @@ $(document).on('scroll', function () {
 function scrollFunction() {
   if ($(document).scrollTop() > 100) {
     $('#nav').removeClass('reg_nav');
-    $('#nav').addClass('scrolled_nav');
+    $('#nav').addClass('scrolled_nav fixed_nav');
     $('#bg_nav').removeClass('reg_nav_bg');
-    $('#bg_nav').addClass('scrolled_nav');
+    $('#bg_nav').addClass('scrolled_nav fixed_nav');
   } else {
-    $('#nav').removeClass('scrolled_nav');
+    $('#nav').removeClass('scrolled_nav fixed_nav');
     $('#nav').addClass('reg_nav');
-    $('#bg_nav').removeClass('scrolled_nav');
+    $('#bg_nav').removeClass('scrolled_nav fixed_nav');
     $('#bg_nav').addClass('reg_nav_bg');
   }
 }
